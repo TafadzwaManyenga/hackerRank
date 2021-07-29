5a3af6ea626044e36c49fb99073f9d15a795bc8e
Tafadzwa Manyenga <30636573+TafadzwaManyenga@users.noreply.github.com>
2021-07-30 01:33:45 +1000

    new solutions

---
+++ magazine/magazine/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'checkMagazine' function below.
+     *
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY magazine
+     *  2. STRING_ARRAY note
+     */
+
+    public static void checkMagazine(List<string> magazine, List<string> note)
+    {
+        string result = "No";
+
+        if (magazine.Count <= 30000 && note.Count <= 30000)
+        {
+
+            List<string> answer = new List<string>();
+            
+            foreach (var b in note)
+            {
+                if (magazine.Contains(b))
+                {
+                    result = "Yes";
+                    magazine.Remove(b);
+                }
+                else
+                {
+                    result = "No";
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        //int m = Convert.ToInt32(firstMultipleInput[0]);
+
+        //int n = Convert.ToInt32(firstMultipleInput[1]);
+
+        //List<string> magazine = Console.ReadLine().TrimEnd().Split(' ').ToList();
+
+        //List<string> note = Console.ReadLine().TrimEnd().Split(' ').ToList();
+
+        List<string> magazine = new List<string>(6);
+        magazine.Add("give");
+        magazine.Add("me");
+        magazine.Add("one");
+        magazine.Add("grand");
+        magazine.Add("today");
+        magazine.Add("please");
+        List<string> note = new List<string>(4);
+        note.Add("give");
+        note.Add("me");
+        note.Add("today");
+        note.Add("please");
+        Result.checkMagazine(magazine, note);
+    }
+}
---
+++ twoStrings/twoStrings/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string twoStrings(string s1, string s2)
+    {
+        char[] a = s1.ToCharArray();
+        char[] b = s2.ToCharArray();
+        string result = "";
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            for (int j = 0; j < b.Length; j++)
+            {
+                if ((a[i] == b[j])&&(a[i]!= '-'))
+                {
+                    result += a[i];
+                    a[i] = '-';b[j] = '-';
+                    break;
+                }
+            }
+        }
+
+        if (result != "")
+        {
+            return "YES";
+        }
+        return "NO";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        //int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        //for (int qItr = 0; qItr < q; qItr++)
+        //{
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = Result.twoStrings(s1, s2);
+
+            Console.WriteLine(result);
+        //}
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
